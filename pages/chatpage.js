3b4f0365747c87447aa4a30bdd7583ad2c2a343b
Richard William <rwilliam.flores@gmail.com>
2024-01-03 19:08:37 +0800

    Refactor chatpage.js component

--- pages/chatpage.js
+++ pages/chatpage.js
@@ -1,27 +1,12 @@
-import Chat from "./Chatbot";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-export default function chatpage () {
- return(
-  
-<div>
-<Header/>
- <Chat />
- <br></br>
- <br></br>
- <br></br>
- <br></br>
- <br></br>
- <br></br>
- <br></br>
- <br></br>
- <br></br>
- <br></br>
- <Footer/>
-</div>
-
-
- );
-
-
+import Chat from './Chatbot';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+export default function chatpage() {
+  return (
+    <div>
+      <Header />
+      <Chat />
+      <Footer />
+    </div>
+  );
 }
