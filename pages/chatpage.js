06ae98ae999bf1d34c1ef8cae715552f9ae46435
Khalil Raddolf Cabatana <cabatana00@gmail.com>
2024-01-03 18:10:39 +0800

    first commit

---
+++ pages/chatpage.js
@@ -0,0 +1,27 @@
+import Chat from "./Chatbot";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+export default function chatpage () {
+ return(
+  
+<div>
+<Header/>
+ <Chat />
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <br></br>
+ <Footer/>
+</div>
+
+
+ );
+
+
+}
