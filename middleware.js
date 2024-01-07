3eceaea965c7b8586b3c24231a98c936e7c8f7b0
Richard William <rwilliam.flores@gmail.com>
2024-01-07 20:14:45 +0800

    Update config matcher in middleware.js

--- middleware.js
+++ middleware.js
@@ -20,5 +20,10 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    '/',
+    '/(api|trpc)(.*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
 };
