d31ce3ceb9c9f8b2949bd8ece6eb39d6225565e2
Richard William <rwilliam.flores@gmail.com>
2024-01-08 10:02:38 +0800

    Revert "Refactor middleware.js and remove vercel.json"
    This reverts commit de17cfcb1f37b218ac8738ba16f49adef59b3e9d.

--- middleware.js
+++ middleware.js
@@ -20,5 +20,11 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: [
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    '/',
+    '/(api|trpc)(.*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/(.*)',
+  ],
 };
